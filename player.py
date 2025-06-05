f8bab54c2502599a688f46a90a4471d99924d934
SCharlwood <samuel_charlwood@hotmail.com>
2025-06-05 16:46:56 +0100

    Added Basic Health system

--- player.py
+++ player.py
@@ -10,6 +10,7 @@ class Player(CircleShape):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
 		self.ShotTimer = 0
+		self.Health =3
 	
 	def triangle(self):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -21,7 +22,14 @@ class Player(CircleShape):
 	
 	
 	def draw(self, screen):
-		pygame.draw.polygon(screen, "white", self.triangle(), 2)
+		if self.Health ==3:
+			SpriteColour = "green"
+		elif self.Health ==2:
+			SpriteColour = "orange"
+		elif self.Health ==1:
+			SpriteColour = "red"
+			
+		pygame.draw.polygon(screen, SpriteColour, self.triangle(), 2)
 	
 	def rotate(self, dt):
 		self.rotation += PLAYER_TURN_SPEED*dt
