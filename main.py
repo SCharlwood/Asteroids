9367a402b120306dda2873ab34adaa14b7ade881
SCharlwood <samuel_charlwood@hotmail.com>
2025-06-05 14:27:23 +0100

    C: add player and player movement

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
+# Internal Timing
 
 
 def main():
 	pygame.init()
 	
-	print("Starting Asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}") 
-	
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0 
 	
-
+	updatable = pygame.sprite.Group()
+	drawable  = pygame.sprite.Group()
+	Player.containers = (updatable,drawable)
 	
+	# Initialise Player character
+	P1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,PLAYER_RADIUS)
 	
 	# Game loop 
 	check = 0
@@ -23,8 +28,18 @@ def main():
 				return
 		screen.fill("black")
 		
+		# Update and Draw all objects
+		updatable.update(dt)
 		
+		for d in drawable:
+			d.draw(screen)
+		
+		dt = clock.tick(60)/1000
 		pygame.display.flip()
+
+
+
+
 
 
 
